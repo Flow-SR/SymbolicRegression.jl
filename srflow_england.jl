5eefe06d9c4699fafa2f7518ab4b3266245173b5
Hao Guo <sinesloop@pku.edu.cn>
2024-04-09 17:05:54 +0800

    Update England Census code
    1. Add intervening opportunities; 
    2. Allow differerent feature for origin and destination.

--- srflow_england.jl
+++ srflow_england.jl
@@ -8,24 +8,40 @@ using FileIO
 
 
 feat = Dict("area_km2"=>3, "respop"=> 4, "employedpop"=> 5, "workpop"=> 6, "households"=>7, "fb_pct"=> 8, "deprived_pct"=> 9, "nonwhite_pct"=> 10, "bach_pct"=> 11, "highsc_pct"=> 12)
-select_feat = ["workpop"]
+select_feat_ori = []
+select_feat_dest = ["respop","workpop"]
+use_dist = true
+use_iowork = true  # intervening opportunity, calculated with workpop
+use_iores = true  # intervening opportunity, calculated with respop
 
 flow_dict = Pickle.load("../data/England/England_msoa_census11_supp3.pkl")
-dist_dict = Pickle.load("../data/England/England_msoa_dist.pkl")
+if use_dist
+    dist_arr = Float64[]
+    dist_dict = Pickle.load("../data/England/England_msoa_dist.pkl")
+end
+if use_iowork
+    iowork_arr = Float64[]
+    iowork_dict = Pickle.load("../data/England/England_msoa_iowork.pkl")
+end
+if use_iores
+    iores_arr = Float64[]
+    iores_dict = Pickle.load("../data/England/England_msoa_iores.pkl")
+end
+
 msoa_units = sort(collect(keys(dist_dict)))
 attrfile = XLSX.readxlsx("../data/England/England_Census11_Attr_Selected.xlsx")
 attrtab = attrfile["attr"]
 
-ori = Int[]
-dest = Int[]
+#ori = Int[]
+#dest = Int[]
 flow = Float64[]
-dist = Float64[]
 geoid2row = Dict{Int,Int}()
 nrows = size(attrtab[:], 1)
-nfeat = size(select_feat,1)
+nfeat_ori = size(select_feat_ori,1)
+nfeat_dest = size(select_feat_dest,1)
 nplaces = size(msoa_units,1)
 
-featarr = [Float64[] for i in 1:2*nfeat]
+featarr = [Float64[] for i in 1:nfeat_ori+nfeat_dest]
 ori_count = [0 for i in 1:nplaces]
 println(nrows-1)
 @assert nplaces==nrows-1
@@ -36,17 +52,31 @@ end
 
 for o in msoa_units
     for d in sort(collect(keys(flow_dict[o])))
-        vol, dis = flow_dict[o][d], dist_dict[o][d]
-        oattr = [attrtab[geoid2row[o], feat[f]] for f in select_feat]
-        dattr = [attrtab[geoid2row[d], feat[f]] for f in select_feat]
         ori_count[geoid2row[o]-1] += 1
-        push!(ori, o)
-        push!(dest, d)
+        vol = flow_dict[o][d]
         push!(flow, vol)
-        push!(dist, dis)
-        for f in 1:nfeat
+        oattr = [attrtab[geoid2row[o], feat[f]] for f in select_feat_ori]
+        dattr = [attrtab[geoid2row[d], feat[f]] for f in select_feat_dest]
+        #push!(ori, o)
+        #push!(dest, d)
+        for f in 1:nfeat_ori
             push!(featarr[f], oattr[f])
-            push!(featarr[nfeat+f], dattr[f])
+        end
+        for f in 1:nfeat_dest
+            push!(featarr[nfeat_ori+f], dattr[f])
+        end
+
+        if use_dist
+            dist = dist_dict[o][d]
+            push!(dist_arr, dist)
+        end
+        if use_iores
+            iores = iores_dict[o][d]
+            push!(iores_arr, iores)
+        end
+        if use_iowork
+            iowork = iowork_dict[o][d]
+            push!(iowork_arr, iowork)
         end
     end
     if geoid2row[o]%100==0
@@ -54,12 +84,13 @@ for o in msoa_units
     end
 end
 y = flow
-X = (D=dist, Wo=featarr[1], Wd=featarr[2]) # TO DO: only for this case
+X = (D=dist_arr, Sr=iores_arr, Sw=iowork_arr, Rd=featarr[1], Wd=featarr[2]) # Need to change everytime 
+
 ori_sep = [sum(ori_count[1:i]) for i in 1:nplaces]
 println(ori_count[1:5])
 println(ori_sep[1:5])
 
-save("eng_supp3_X_dist_odwp.jld2", "X", X)
+save("eng_supp3_X_dist_iorw_drw.jld2", "X", X)
 save("eng_supp3_Y.jld2", "y", y)
 save("eng_supp3_sep.jld2", "sep", ori_sep)
 
