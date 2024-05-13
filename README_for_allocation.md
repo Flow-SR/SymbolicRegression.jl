c297070d09ff71ffe0f6322780ecdc566235e564
Hao Guo <sinesloop@pku.edu.cn>
2024-05-13 20:04:11 +0800

    Update UK census code and doc

--- README_for_allocation.md
+++ README_for_allocation.md
@@ -4,7 +4,7 @@
 
 - OptionsModule
 
---  **Options** : The construction function is in Options.jl and the definition is in Optionstruct.jl. Attributes `allocation`, `eval_probability`, `ori_sep`, `num_places` are added.  
+--  **Options** : The construction function is in Options.jl and the definition is in Optionstruct.jl. Attributes `allocation`, `eval_probability`, `ori_sep`, `num_places`,  `optimize_hof` are added.  
 
 -- In allocation mode, `ori_sep` is required as n-dim vector, where n is the number of places; dataset entry `ori_sep[i-1]+1:ori_sep[i]` corresponds to flows with origin `i`. Alternatively, you may input n*n `adjmatrix`, which is transformed into `ori_sep`. `num_places` will be calculated automatically.  
 
@@ -14,6 +14,9 @@
 -- **_eval_loss**: Perform probability normalization if `allocation`. If `eval_probability`, do not multiply total outflow.  
 -- **batch_sample**: Sample from `1:num_places` instead of `1:dataset.n` if allocation.
 
+- SymbolicRegressionModule
+-- **_equation_search**: if `optimize_hof`, Hall-of-Fame equations will be optimized with entire dataset (even if `batching=true`) after the last `s_r_cycle`.
+
 ## Usage
 
 - Copy '/src/' directory into the project, rename it as '/lib/', then use the following to import:  
@@ -22,8 +25,3 @@
 (No need to install the "SymbolicRegression" library)
 - Precomilation may fail if dependencies are not installed. Use `add "packageXX"` in Pkg mode to fix.
 - Sample Code in `srflow_basictest.jl`
-
-## TODO
-
-- Migrate the dataset processing codes to Julia (Maybe random data first).
-- Debug.
--- srflow_england.jl
+++ srflow_england.jl
@@ -2,38 +2,42 @@ import MLJ: machine, fit!, predict, report
 push!(LOAD_PATH, "./lib")
 using SymbolicRegression: SRRegressor
 using XLSX
-using Pickle
 using Dates
 using FileIO
 
+level= "mlad" # "msoa" or "mlad"
 
-feat = Dict("area_km2"=>3, "respop"=> 4, "employedpop"=> 5, "workpop"=> 6, "households"=>7, "fb_pct"=> 8, "deprived_pct"=> 9, "nonwhite_pct"=> 10, "bach_pct"=> 11, "highsc_pct"=> 12)
-select_feat_ori = []
-select_feat_dest = ["respop","workpop"]
+#= First make data
+using Pickle
+if level == "msoa"
+    feat = Dict("area_km2"=>3, "respop"=> 4, "employedpop"=> 5, "workpop"=> 6, "households"=>7, "fb_pct"=> 8, "deprived_pct"=> 9, "nonwhite_pct"=> 10, "bach_pct"=> 11, "highsc_pct"=> 12)
+elseif level == "mlad"
+    feat = Dict("respop"=> 4, "workpop"=> 5)
+end
+
+select_feat_ori = ["respop", "workpop"]
+select_feat_dest = ["respop", "workpop"]
 use_dist = true
 use_iowork = true  # intervening opportunity, calculated with workpop
 use_iores = true  # intervening opportunity, calculated with respop
 
-flow_dict = Pickle.load("../data/England/England_msoa_census11_supp3.pkl")
-if use_dist
-    dist_arr = Float64[]
-    dist_dict = Pickle.load("../data/England/England_msoa_dist.pkl")
-end
+flow_dict = Pickle.load("../data/England/England_"* level *"_census11_supp3.pkl")
+dist_arr = Float64[]
+dist_dict = Pickle.load("../data/England/England_"* level *"_dist.pkl")
+
 if use_iowork
     iowork_arr = Float64[]
-    iowork_dict = Pickle.load("../data/England/England_msoa_iowork.pkl")
+    iowork_dict = Pickle.load("../data/England/England_"* level *"_iowork.pkl")
 end
 if use_iores
     iores_arr = Float64[]
-    iores_dict = Pickle.load("../data/England/England_msoa_iores.pkl")
+    iores_dict = Pickle.load("../data/England/England_"* level *"_iores.pkl")
 end
 
-msoa_units = sort(collect(keys(dist_dict)))
-attrfile = XLSX.readxlsx("../data/England/England_Census11_Attr_Selected.xlsx")
+units = sort(collect(keys(dist_dict)))
+attrfile = XLSX.readxlsx("../data/England/England_"* level *"_census11_attr.xlsx")
 attrtab = attrfile["attr"]
 
-#ori = Int[]
-#dest = Int[]
 flow = Float64[]
 geoid2row = Dict{Int,Int}()
 nrows = size(attrtab[:], 1)
@@ -41,7 +45,8 @@ nfeat_ori = size(select_feat_ori,1)
 nfeat_dest = size(select_feat_dest,1)
 nplaces = size(msoa_units,1)
 
-featarr = [Float64[] for i in 1:nfeat_ori+nfeat_dest]
+ofeatarr = [Float64[] for i in 1:nfeat_ori]
+dfeatarr = [Float64[] for i in 1:nfeat_dest]
 ori_count = [0 for i in 1:nplaces]
 println(nrows-1)
 @assert nplaces==nrows-1
@@ -50,20 +55,18 @@ for r in 2:nrows
     geoid2row[parse(Int,attrtab[r, 1][end-5:end])] = r
 end
 
-for o in msoa_units
+for o in units
     for d in sort(collect(keys(flow_dict[o])))
         ori_count[geoid2row[o]-1] += 1
         vol = flow_dict[o][d]
         push!(flow, vol)
         oattr = [attrtab[geoid2row[o], feat[f]] for f in select_feat_ori]
         dattr = [attrtab[geoid2row[d], feat[f]] for f in select_feat_dest]
-        #push!(ori, o)
-        #push!(dest, d)
         for f in 1:nfeat_ori
-            push!(featarr[f], oattr[f])
+            push!(ofeatarr[f], oattr[f])
         end
         for f in 1:nfeat_dest
-            push!(featarr[nfeat_ori+f], dattr[f])
+            push!(dfeatarr[f], dattr[f])
         end
 
         if use_dist
@@ -72,10 +75,16 @@ for o in msoa_units
         end
         if use_iores
             iores = iores_dict[o][d]
+            if level=="msoa"
+                iores += attrtab[geoid2row[o], feat["respop"]] 
+            end
             push!(iores_arr, iores)
         end
         if use_iowork
-            iowork = iowork_dict[o][d]
+            iowork = iowork_dict[o][d] 
+            if level=="msoa"
+                iowprk += attrtab[geoid2row[o], feat["workpop"]] 
+            end
             push!(iowork_arr, iowork)
         end
     end
@@ -84,42 +93,44 @@ for o in msoa_units
     end
 end
 y = flow
-X = (D=dist_arr, Sr=iores_arr, Sw=iowork_arr, Rd=featarr[1], Wd=featarr[2]) # Need to change everytime 
+#X = (D=dist_arr,  Wo=ofeatarr[1], Wd=dfeatarr[1])
+X = (D=dist_arr, Sr=iores_arr, Sw=iowork_arr, Ro=ofeatarr[1], Wo=ofeatarr[2], Rd=dfeatarr[1], Wd=dfeatarr[2]) # Need to change everytime making new data 
 
 ori_sep = [sum(ori_count[1:i]) for i in 1:nplaces]
 println(ori_count[1:5])
 println(ori_sep[1:5])
 
-save("eng_supp3_X_dist_iorw_drw.jld2", "X", X)
-save("eng_supp3_Y.jld2", "y", y)
-save("eng_supp3_sep.jld2", "sep", ori_sep)
+save("eng_"*level*"_supp3_X_dist_miorw_odrw.jld2", "X", X)
+save("eng_"*level*"_supp3_Y.jld2", "y", y)
+save("eng_"*level*"_supp3_sep.jld2", "sep", ori_sep)
 
-#=
-X = load("eng_supp3_X_dist_odwp.jld2", "X")
-y = load("eng_supp3_Y.jld2", "y")
-ori_sep = load("eng_supp3_sep.jld2", "sep")
-=#
+=# # Load Existing Data
+X = load("eng_"*level*"_supp3_X_dist_odw.jld2", "X")
+y = load("eng_"*level*"_supp3_Y.jld2", "y")
+ori_sep = load("eng_"*level*"_supp3_sep.jld2", "sep")
+
 
 timestamp = Dates.format(Dates.now(),"yyyymmddHHMM")[3:end]
 model = SRRegressor(
-    niterations=100,
+    niterations=5,
     binary_operators=[+, -, *, /, ^],
     unary_operators=[exp, log],
     complexity_of_operators=[exp => 2, log => 2],
     constraints=[(^)=>(-1, 1), exp => 1, log => 1],
     allocation=true,
+    optimize_hof=true, 
     ori_sep=ori_sep,
-    batching=true,
-    batch_size=40,
-    #optimizer_probability=1.0,
-    #optimizer_iterations=20,
-    output_file="hall_of_fame_" * timestamp * ".csv",
+    output_file="hof_ukcensus_" *level *"_"* timestamp * ".csv",
 )
 
+if level=="msoa"
+    model.batching=true
+    model.batch_size=40
+end
 
 mach = machine(model, X, y)
 fit!(mach)
 report(mach)
 
-yp = predict(mach, X)
+# yp = predict(mach, X)
 # yp = predict(mach, (data=X, idx=2))
